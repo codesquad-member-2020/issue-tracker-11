849f29e0f2102f83e2749aa6ddb8a68fef998774
David Chung <david215@berkeley.edu>
2020-06-09 22:00:24 +0900

    :tada: Initialize project

---
+++ server/src/main/java/io/codesquad/group11/issuetracker/IssuetrackerApplication.java
@@ -0,0 +1,13 @@
+package io.codesquad.group11.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuetrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssuetrackerApplication.class, args);
+    }
+
+}
