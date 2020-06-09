7228138ba4a67545b59215cd15d216d531cbe74b
idevjinie <idevjinie@gmail.com>
2020-06-10 01:29:02 +0900

    [#3] :sparkles: 버튼의 cornerRadius 설정

--- Jinie/IssueTracker/IssueTracker/Sources/ViewControllers/SignInViewController.swift
+++ Jinie/IssueTracker/IssueTracker/Sources/ViewControllers/SignInViewController.swift
@@ -9,8 +9,19 @@
 import UIKit
 
 class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var signInButton: UIButton!
+    
+    private let cornerRadius: CGFloat = 5.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureButton()
+    }
+    
+    private func configureButton() {
+        signInButton.layer.cornerRadius = cornerRadius
+        signInButton.layer.masksToBounds = true
     }
 }
