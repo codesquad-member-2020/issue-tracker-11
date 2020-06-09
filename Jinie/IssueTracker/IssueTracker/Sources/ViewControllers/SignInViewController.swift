0dd1b86e93be8210a525b7a259fca50343b7c9f2
idevjinie <idevjinie@gmail.com>
2020-06-09 19:38:13 +0900

    [#3] :sparkles: 스토리보드에 화면 관련 인터페이스 구성

---
+++ Jinie/IssueTracker/IssueTracker/Sources/ViewControllers/SignInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInViewController.swift
+//  IssueTracker
+//
+//  Created by jinie on 2020/06/09.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
