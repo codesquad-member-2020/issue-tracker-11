3d11e5e3641bf04076e84210ab7376a7b95f5c4a
idevjinie <idevjinie@gmail.com>
2020-06-11 15:22:48 +0900

    [#7] 테스트에 가짜 객체로 사용될 FakeASWebAuthenticationSession 클래스 추가

---
+++ Jinie/IssueTracker/IssueTrackerTests/Sources/Mocks/FakeASWebAuthenticationSession.swift
@@ -0,0 +1,20 @@
+//
+//  FakeASWebAuthenticationSession.swift
+//  IssueTrackerTests
+//
+//  Created by jinie on 2020/06/11.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import XCTest
+@testable import IssueTracker
+
+final class FakeASWebAuthenticationSession: ASWebAuthenticationSessionProtocol {
+    
+    let callbackURL = URL(string: "io.codesquad.issuetracker://example.com/auth?token=test")
+    
+    @discardableResult
+    init(url URL: URL, callbackURLScheme: String?, completionHandler: @escaping (URL?, Error?) -> Void) {
+        completionHandler(callbackURL, nil)
+    }
+}
