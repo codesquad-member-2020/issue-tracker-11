e3a5b80b9657deecfa3fa938848879a04c740870
idevjinie <idevjinie@gmail.com>
2020-06-11 15:14:03 +0900

    [#7] ASWebAuthenticationSession 클래스와 같은 생성자를 정의하는 ASWebAuthenticationSessionProtocol 추가

---
+++ Jinie/IssueTracker/IssueTracker/Sources/Protocols/ASWebAuthenticationSessionProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ASWebAuthenticationSessionProtocol.swift
+//  IssueTracker
+//
+//  Created by jinie on 2020/06/11.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import AuthenticationServices
+
+protocol ASWebAuthenticationSessionProtocol {
+
+    init(url URL: URL, callbackURLScheme: String?, completionHandler: @escaping (URL?, Error?) -> Void)
+}
