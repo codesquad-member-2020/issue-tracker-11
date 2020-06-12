2392cb1eb5cd68e4cae5a3113a6c09ec80531d56
seizze <cwcwc3@gmail.com>
2020-06-12 16:57:49 +0900

    [#13] :white_check_mark: Improve readability of test code

--- Heidi/IssueTracker/IssueTrackerTests/GitHubAuthTaskTests.swift
+++ Heidi/IssueTracker/IssueTrackerTests/GitHubAuthTaskTests.swift
@@ -12,7 +12,8 @@ import XCTest
 final class GitHubAuthTaskTests: XCTestCase {
     func test_GitHubAuth_Fail_With_URLNotFound() {
         let exp = expectation(description: "Callback URL이 없다는 에러로 실패함")
-        GitHubAuthTask(dispatcher: AuthenticationDispatcherErrorStub()).perform(GitHubAuthRequest()) {
+        let dispatcherStub = AuthenticationDispatcherErrorStub()
+        GitHubAuthTask(dispatcher: dispatcherStub).perform(GitHubAuthRequest()) {
             if case let .failure(error) = $0 {
                 XCTAssertEqual(error as! AuthenticationError, AuthenticationError.urlNotFound)
                 exp.fulfill()
@@ -23,7 +24,8 @@ final class GitHubAuthTaskTests: XCTestCase {
     
     func test_GitHubAuth_Fail_With_TokenNotFound() {
         let exp = expectation(description: "토큰을 찾지 못했다는 에러로 실패함")
-        GitHubAuthTask(dispatcher: AuthenticationDispatcherNoTokenStub()).perform(GitHubAuthRequest()) {
+        let dispatcherStub = AuthenticationDispatcherNoTokenStub()
+        GitHubAuthTask(dispatcher: dispatcherStub).perform(GitHubAuthRequest()) {
             if case let .failure(error) = $0 {
                 XCTAssertEqual(error as! AuthenticationError, AuthenticationError.tokenNotFound)
                 exp.fulfill()
@@ -34,7 +36,8 @@ final class GitHubAuthTaskTests: XCTestCase {
     
     func test_GitHubAuth_Success() {
         let exp = expectation(description: "토큰 파싱 성공함")
-        GitHubAuthTask(dispatcher: AuthenticationDispatcherSuccessStub()).perform(GitHubAuthRequest()) {
+        let dispatcherStub = AuthenticationDispatcherSuccessStub()
+        GitHubAuthTask(dispatcher: dispatcherStub).perform(GitHubAuthRequest()) {
             if case let .success(token) = $0 {
                 XCTAssertEqual(token, "jwtjwt")
                 exp.fulfill()
