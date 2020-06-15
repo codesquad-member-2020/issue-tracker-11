6a71403e23f110e783d32cd3be6d08615d8e9cea
seizze <cwcwc3@gmail.com>
2020-06-15 22:42:35 +0900

    [#15] :sparkles: Add new dashboard VC

---
+++ Heidi/IssueTracker/IssueTracker/Controllers/DashboardViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DashboardViewController.swift
+//  IssueTracker
+//
+//  Created by Chaewan Park on 2020/06/15.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+final class DashboardViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
