9e1ce0805f23767684181be44e4a01170aeae5f4
seizze <cwcwc3@gmail.com>
2020-06-16 01:31:08 +0900

    [#16] :white_check_mark: Create new test case

---
+++ Heidi/IssueTracker/IssueTrackerTests/LoginStorageTaskTests.swift
@@ -0,0 +1,13 @@
+//
+//  LoginStorageTaskTests.swift
+//  IssueTrackerTests
+//
+//  Created by Chaewan Park on 2020/06/16.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import XCTest
+
+class LoginStorageTaskTests: XCTestCase {
+    
+}
