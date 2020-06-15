60389d0fc70ae577d12ab3f207cda685abb1f828
seizze <cwcwc3@gmail.com>
2020-06-15 22:37:05 +0900

    [#15] :sparkles: Add some utils for loading VCs

---
+++ Heidi/IssueTracker/IssueTracker/Utils/Identifiable.swift
@@ -0,0 +1,19 @@
+//
+//  Identifiable.swift
+//  IssueTracker
+//
+//  Created by Chaewan Park on 2020/06/15.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+protocol Identifiable {
+    static var identifier: String { get }
+}
+
+extension Identifiable {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
