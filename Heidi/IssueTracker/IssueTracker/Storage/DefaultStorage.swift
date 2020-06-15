3d38bd39ec699e5ce951ffa9af26ecef5b46c544
seizze <cwcwc3@gmail.com>
2020-06-15 19:47:53 +0900

    [#15] :sparkles: Implement local storage

---
+++ Heidi/IssueTracker/IssueTracker/Storage/DefaultStorage.swift
@@ -0,0 +1,32 @@
+//
+//  DefaultStorage.swift
+//  IssueTracker
+//
+//  Created by Chaewan Park on 2020/06/15.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+struct DefaultStorage<Content>: Storage {
+    private let storage: UserDefaults
+    
+    init(with storage: UserDefaults = .standard) {
+        self.storage = storage
+    }
+    
+    func read(for key: StorageKey<Content>) -> Content? {
+        return storage[key]
+    }
+    
+    func write(_ newValue: Content, for key: StorageKey<Content>) {
+        storage[key] = newValue
+    }
+}
+
+extension UserDefaults {
+    subscript<Content>(key: StorageKey<Content>) -> Content? {
+        get { return object(forKey: key.value) as? Content }
+        set { set(newValue, forKey: key.value) }
+    }
+}
