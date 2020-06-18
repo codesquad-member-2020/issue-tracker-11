b1e8bfba05bedcbcdcfd643740abf4592ead53cc
idevjinie <idevjinie@gmail.com>
2020-06-18 22:35:47 +0900

    [#18] Apple ID를 사용하여 앱 계정 생성

--- Jinie/IssueTracker/IssueTracker/Sources/ViewControllers/SignInViewController.swift
+++ Jinie/IssueTracker/IssueTracker/Sources/ViewControllers/SignInViewController.swift
@@ -14,6 +14,7 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var appleAuthButtonView: UIView!
     
     private var appleAuthButton: ASAuthorizationAppleIDButton!
+    private var cornerRadius: CGFloat = 5.0
     
     @IBAction func gitHubAuthButtonTapped(_ sender: UIButton) {
         signInWithGitHub()
@@ -26,27 +27,8 @@ class SignInViewController: UIViewController {
         configureAppleAuthButton()
     }
     
-    private func signInWithGitHub() {
-        let dispatcher = GitHubAuthDispatcher()
-        let task = GitHubAuthTask(dispatcher: dispatcher)
-        
-        task.perform(request: GitHubAuthRequest()) { result in
-            if case .success(let token) = result {
-                UserDefaults.standard.set(token, forKey: "token")
-                self.presentTabBarController()
-            }
-        }
-    }
-    
-    private func presentTabBarController() {
-        let tabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
-        tabBarController.modalPresentationStyle = .fullScreen
-        tabBarController.modalTransitionStyle = .crossDissolve
-        present(tabBarController, animated: true, completion: nil)
-    }
-    
     private func configureGitHubAuthButton() {
-        gitHubAuthButton.layer.cornerRadius = 6.0
+        gitHubAuthButton.layer.cornerRadius = cornerRadius
         gitHubAuthButton.layer.masksToBounds = true
     }
     
@@ -57,8 +39,44 @@ class SignInViewController: UIViewController {
             appleAuthButton = ASAuthorizationAppleIDButton(authorizationButtonType: .default, authorizationButtonStyle: .black)
         }
         
+        appleAuthButton.addTarget(self, action: #selector(signInWithApple), for: .touchUpInside)
         appleAuthButton.center = appleAuthButtonView.center
         appleAuthButton.frame = appleAuthButtonView.bounds
+        appleAuthButton.cornerRadius = cornerRadius
         appleAuthButtonView.addSubview(appleAuthButton)
+    }
+    
+    private func signInWithGitHub() {
+        let gitHubAuthDispatcher = GitHubAuthDispatcher()
+        let gitHubAuthTask = GitHubAuthTask(dispatcher: gitHubAuthDispatcher)
+        
+        gitHubAuthTask.perform(request: GitHubAuthRequest()) { result in
+            if case .success(let token) = result {
+                UserDefaults.standard.set(token, forKey: "token")
+                self.presentTabBarController()
+            }
+        }
+    }
+    
+    @objc func signInWithApple() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    private func presentTabBarController() {
+        let tabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
+        tabBarController.modalPresentationStyle = .fullScreen
+        tabBarController.modalTransitionStyle = .crossDissolve
+        present(tabBarController, animated: true, completion: nil)
+    }
+}
+
+extension SignInViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
     }
 }
