f3522334782454a8c703c8aa24f971ae5ba9f975
seizze <cwcwc3@gmail.com>
2020-06-24 14:16:27 +0900

    [#25] :sparkles: Add new view controller

---
+++ Heidi/IssueTracker/IssueTracker/Controllers/IssueDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  IssueDetailViewController.swift
+//  IssueTracker
+//
+//  Created by Chaewan Park on 2020/06/24.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class IssueDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
